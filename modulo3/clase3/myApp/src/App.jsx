192578fac5850706568f1429e9b62cd428fdeba7
Angatupyry <fierrofenix@gmail.com>
2024-10-01 19:31:22 -0400

    Ecommerce
    Signed-off-by: Angatupyry <fierrofenix@gmail.com>

--- modulo3/clase3/myApp/src/App.jsx
+++ modulo3/clase3/myApp/src/App.jsx
@@ -3,8 +3,13 @@ import Hero from "./components/Hero";
 import { Navbar } from "./components/Navbar";
 
 export default function App() {
+  const [open, setOpen] = useState(false);
   return (
     <>
+      <img src='' alt='' onClick={() => setOpen(!open)} />
+
+      {open && <Card />}
+
       <Navbar />
       <Hero />
     </>
